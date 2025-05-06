652299a7851dc14a97c8e32cb713a585182f8c52
Hariharan4902 <hhari2804@gmail.com>
2025-05-06 15:47:56 +0530

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,47 @@
-const DOB = "14/10/2001";
-const AGE = "2001";
+ /* const DOB = "14/10/2001";
+const AGE = calAge(DOB);
 
-console.log(DOB);
+console.log(AGE); 
+
+let pi = 3.14;
+let prime =7;
+
+console.log(prime);
+
+console.log(typeof prime);
+
+// Special Numeric Value
+
+let infi = Infinity;
+
+console.log(infi);
+
+let temp = NaN;
+
+temp = NaN ** 0;
+console.log(temp);
+
+// BigInt
+
+let limit = 900771399254704991n;
+
+console.log(limit + 1n);
+console.log(limit + 2n);
+console.log(limit + 3n);
+
+console.log(typeof limit);
+ */
+//String
+
+let para = `This is bat man's house. Hari said " I'm bat man"`;
+
+let name = "Hari";
+let sentence =`My Name is ${name}`;
+
+console.log(sentence);
+
+let carName = "BMW";
+
+carName = 4330;
+
+console.log(typeof carName);  
