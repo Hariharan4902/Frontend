6ffb66ac842a69518595bc87b3075dff0cac67d1
Hariharan4902 <hhari2804@gmail.com>
2025-05-07 17:36:56 +0530

    Add files via upload

---
+++ operator.js
@@ -0,0 +1,39 @@
+//Type Conersion
+
+let age = "23";
+console.log(typeof age);
+age = Number(age);
+
+console.log(age+3);
+
+
+//Operator
+
+//Unary
+
+let x=-1;
+x=-x;
+console.log(x);
+
+//Binary
+
+/* let a=5, b=5;
+let sum = a+b;
+console.log(sum);
+
+let c='Hi', d='Hello';
+let sum1 = c+d;
+console.log(sum);
+
+let a=b=c=5;
+console.log(a,b,c);
+
+ */
+
+//INcrement Decrement
+
+let a= 12 ; b=34;
+
+let c= ++a + b--;
+
+console.log(c);
